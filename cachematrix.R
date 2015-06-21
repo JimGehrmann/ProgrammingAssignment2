03516e61ecb4f908441ee51a60e08c7e92d0e268
JimGehrmann <jim_gehrmann@hotmail.com>
2015-06-20 20:20:24 -0700

    Programming Assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,40 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## makeCacheMatrix creates four functions, get, set,
+## setmatinv, and getmatinv
+## get will get the value of a matrix (square and invertible)
+## set will change values in a matrix
+## getmatinv displays the inverse of a square invertible matrix
+## setmatinv will change values of a matrix inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+matinv<-NULL
+  set <- function(y) {
+    x <<- y
+    matinv <<- NULL
+  }
+  get <- function() x
+  setmatinv <- function(inverser) matinv <<- inverser
+  getmatinv <- function() matinv
+  
+  list(set = set, get = get,setmatinv=setmatinv,getmatinv=getmatinv)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve will compute the inverse of a square invertible matrix if
+## the inverse hasn't been created and cached.
+## If the inverse has been cached, a statement saying the matrix inverse has
+## been found will be displayed
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+
+  matinv<-x$getmatinv()
+  if(!is.null(matinv))
+  {message("getting cached data")
+    return(matinv)
+    
+  }
+  data<-x$get()
+  matinv<-solve(data,...)
+  x$setmatinv(matinv)
+  matinv
+
 }
